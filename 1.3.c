dd03509cb9479ba3a6efb187bf6a212642edc9a1
kkoolwal <kushalkoolwal@gmail.com>
2013-07-23 10:10:54 -0700

    added new method to remove duplicate characters.

--- 1.3.c
+++ 1.3.c
@@ -3,6 +3,37 @@
 
 #define BUFF_SIZE 	20
 
+
+void remove_dup_v2 (char *s) {
+
+	int i, j, k;
+	char ch;
+
+	i=j=k=0;
+
+	/* scan all characters in the string */	
+	while (s[i] != '\0') {
+		ch = s[i];
+		/* scan all characters from 'i' onwards to see if 'ch' is present */
+		j = i + 1;
+		while (s[j] != '\0') {
+			if (s[j] == ch) {
+				/* shift characters left by one, hence emoving the duplicates */
+				k = j;
+				while (s[k] != '\0') {
+					s[k] = s[k+1];
+					k++;
+				}
+			}
+			else {
+				j++;
+			}
+		}
+		i++;
+	}
+
+}
+
 /* remove duplicate characters from string without additional array
    scan every character in the string and compare it with all the previous
    characters. If duplicate we skip the character, otherwise copy it 	
@@ -26,7 +57,7 @@ void remove_dup (char *s) {
 				break;
 		}
 		#if DEBUG
-		printf("j=%d tail=%d", j, tail);
+		printf("j=%d tail=%d\n", j, tail);
 		#endif
 		if (j == tail) {
 			#if DEBUG
@@ -46,7 +77,7 @@ void main (void) {
 	printf("Enter a string to reverse it:");
 	/* scanf adds '\0' character automatically */
 	scanf("%s", buffer);
-	remove_dup(buffer);
+	remove_dup_v2(buffer);
 	printf("String w/o duplicate characters: %s\n", buffer);
 	return;
 }
